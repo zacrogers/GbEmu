6b40e69766abedfb18f2e2e0f0b964e63fcaddc9
zacrogers <z.m.rogers@gmail.com>
2023-06-23 16:03:02 +1200

    add desktop platform

---
+++ emu_core/inc/graphics.hh
@@ -0,0 +1,8 @@
+#pragma once
+
+class Graphics
+{
+  public:
+    virtual bool init() = 0;
+    virtual void draw() = 0;
+};
--- run.sh
+++ run.sh
@@ -7,14 +7,28 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+
+if [[ $1 == "DESKTOP" ]]; then
+
+cd desktop
+
 cmake .
 
 echo -e "${BLUE}*********************************${NC}"
 echo -e "${RED}**********   ${GREEN}Building   ${RED}*********${NC}"
 echo -e "${BLUE}*********************************${NC}"
+
 make
 
 echo -e "${BLUE}*********************************${NC}"
 echo -e "${RED}**********    ${GREEN}Running   ${RED}*********${NC}"
 echo -e "${BLUE}*********************************${NC}"
-./GbEmu '../roms/cpu_instrs.gb'
+
+./GameboyEmulatorDesktop '../roms/cpu_instrs.gb'
+
+elif [[ $1 == "EMBEDDED" ]]; then
+
+cd embedded
+west build -p auto -b esp32
+
+fi
