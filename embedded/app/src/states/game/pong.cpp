118af976e5b39527f9ab5d49605027039378689a
zacrogers <z.m.rogers@gmail.com>
2024-08-16 19:02:08 +1200

    tidy sliders

--- embedded/app/src/states/game/pong.cpp
+++ embedded/app/src/states/game/pong.cpp
@@ -4,6 +4,51 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(pong_game, LOG_LEVEL_DBG);
 
+// static lv_obj_t * create_text(lv_obj_t * parent, const char * icon, const char * txt,
+//                               lv_menu_builder_variant_t builder_variant)
+// {
+//     lv_obj_t * obj = lv_menu_cont_create(parent);
+
+//     lv_obj_t * img = NULL;
+//     lv_obj_t * label = NULL;
+
+//     if(icon) {
+//         img = lv_image_create(obj);
+//         lv_image_set_src(img, icon);
+//     }
+
+//     if(txt) {
+//         label = lv_label_create(obj);
+//         lv_label_set_text(label, txt);
+//         lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
+//         lv_obj_set_flex_grow(label, 1);
+//     }
+
+//     if(builder_variant == LV_MENU_ITEM_BUILDER_VARIANT_2 && icon && txt) {
+//         lv_obj_add_flag(img, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
+//         lv_obj_swap(img, label);
+//     }
+
+//     return obj;
+// }
+
+static lv_obj_t * create_slider(lv_obj_t * parent, int32_t min, int32_t max,
+                                int32_t val)
+{
+    // lv_obj_t * obj = create_text(parent, icon, txt, LV_MENU_ITEM_BUILDER_VARIANT_2);
+
+    lv_obj_t * slider = lv_slider_create(parent);
+    lv_obj_set_flex_grow(slider, 1);
+    lv_slider_set_range(slider, min, max);
+    lv_slider_set_value(slider, val, LV_ANIM_OFF);
+
+    // if(icon == NULL) {
+    //     lv_obj_add_flag(slider, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
+    // }
+
+    return slider;
+}
+
 namespace game
 {
 
@@ -140,12 +185,8 @@ void PongGame::init_settings_menu()
     // label = lv_label_create(cont);
     // lv_label_set_text(label, "Item 1");
 
-    slider = lv_slider_create(cont);
+    slider = create_slider(cont, 1, 10, slider_val);
     lv_group_add_obj(menu_slider_group, slider);
-    lv_obj_set_flex_grow(slider, 1);
-    lv_slider_set_range(slider, 1, 10);
-    lv_slider_set_value(slider, slider_val, LV_ANIM_OFF);
-    lv_obj_add_flag(slider, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
     lv_obj_add_style(slider, &style_base, 0);
     lv_obj_add_style(slider, &style_focused, LV_STATE_FOCUSED);
 
@@ -153,12 +194,8 @@ void PongGame::init_settings_menu()
     // label = lv_label_create(cont);
 
     // lv_label_set_text(label, "Item 2");
-    sliderb = lv_slider_create(cont);
+    sliderb = create_slider(cont, 1, 10, slider_val);
     lv_group_add_obj(menu_slider_group, sliderb);
-    lv_obj_set_flex_grow(sliderb, 1);
-    lv_slider_set_range(sliderb, 1, 10);
-    lv_slider_set_value(sliderb, slider_valb, LV_ANIM_OFF);
-    lv_obj_add_flag(sliderb, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
     lv_obj_add_style(sliderb, &style_base, 0);
     lv_obj_add_style(sliderb, &style_focused, LV_STATE_FOCUSED);
 
@@ -166,12 +203,8 @@ void PongGame::init_settings_menu()
     // label = lv_label_create(cont);
 
     // lv_label_set_text(label, "Item 3");
-    sliderc = lv_slider_create(cont);
+    sliderc = create_slider(cont, 1, 10, slider_val);
     lv_group_add_obj(menu_slider_group, sliderc);
-    lv_obj_set_flex_grow(sliderc, 1);
-    lv_slider_set_range(sliderc, 1, 10);
-    lv_slider_set_value(sliderc, slider_valc, LV_ANIM_OFF);
-    lv_obj_add_flag(sliderc, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
     lv_obj_add_style(sliderc, &style_base, 0);
     lv_obj_add_style(sliderc, &style_focused, LV_STATE_FOCUSED);
 
